9f477d9dc79071a0f01dd62412d16013a5384330
Jamie White <106563693+JWhite212@users.noreply.github.com>
2024-01-05 22:15:04 +0000

    Finished Intro section

--- components/intro.tsx
+++ components/intro.tsx
@@ -2,13 +2,20 @@
 
 import { motion } from "framer-motion";
 import Image from "next/image";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { FaGithubSquare } from "react-icons/fa";
+import { HiDownload } from "react-icons/hi";
 
 export default function Intro() {
   return (
-    <section>
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
       <div className="flex items-center justify-center">
         <div className="relative">
-          <motion.div>
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: "tween", duration: 0.2 }}>
             <Image
               src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
               alt="Ricardo portrait"
@@ -19,9 +26,57 @@ export default function Intro() {
               className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
             />
           </motion.div>
-          <span className="absolute text-4xl bottom-0 right-0">✌</span>
+          <motion.span
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 100,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+            className="absolute text-4xl bottom-0 right-0">
+            ✌
+          </motion.span>
         </div>
       </div>
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}>
+        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a
+        <span className="font-bold"> full-stack developer</span> with
+        <span className="font-bold"> 8 years</span> of experience. I enjoy
+        building <span className="italic">sites & apps</span>. My focus is
+        <span className="underline"> React (Next.js)</span>.
+      </motion.h1>
+
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}
+        className="flex flex-col sm:flex-row justify-center items-center gap-2 px-4 text-lg font-medium">
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition">
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1" />
+        </Link>
+        <a
+          className="group bg-white text-black px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 cursor-pointer border-black-/10 transition"
+          href="/CV.pdf">
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+        <a className="bg-white text-gray-700 p-4 flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 cursor-pointer border-black-/10 transition" href="https://www.linkedin.com/in/jamie-white-swe/">
+          {" "}
+          <BsLinkedin />
+        </a>
+        <a className="bg-white text-gray-700 p-4 flex items-center gap-2 rounded-full outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 cursor-pointer border-black-/10 transition" href="https://github.com/JWhite212">
+          {" "}
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 }

5f1b24da7ac67ab94d55174c5be73cfd6f530ae4
Jamie White <106563693+JWhite212@users.noreply.github.com>
2024-01-05 19:47:13 +0000

    Added Header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/header";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -15,10 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={"${inter.className} bg-gray-50 text-gray-950 relative"}>
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blurr-[10rem] sm:w-[68.75rem]"></div>
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] -z-10 rounded-full blurr-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left:-[-28rem] xl:left:-[-15rem] 2xl:left:-[-5rem]"></div>
+      <body className={"${inter.className}  bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90"}>
+        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[-11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[90.75rem] dark:bg-[#946263]"></div>
+        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
 
+        <Header />
         {children}
       </body>
     </html>

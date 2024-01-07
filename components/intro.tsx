8d2d4fbeeefd57ea96496538a888b1285f49990c
Jamie White <106563693+JWhite212@users.noreply.github.com>
2024-01-07 12:11:16 +0000

    update

--- components/intro.tsx
+++ components/intro.tsx
@@ -17,7 +17,7 @@ export default function Intro() {
   return (
     <section
       ref={ref}
-      className="mb-28 mt-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+      className="mb-28 mt-18 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
       id="home">
       <div className="flex items-center justify-center">
         <div className="relative">

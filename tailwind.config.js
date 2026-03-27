0637c0b6ec57b2a6580730bccf7801f7b7f99658
Jamie White <106563693+JWhite212@users.noreply.github.com>
2026-03-27 05:48:21 +0000

    fix: small changes to tailwind.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,34 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
-<<<<<<< HEAD
       fontFamily: {
         sans: ["var(--font-space-grotesk)", "sans-serif"],
         mono: ["var(--font-ibm-plex-mono)", "monospace"],
-=======
-      colors: {
-        primary: {
-          DEFAULT: '#6366f1', // Indigo-500
-          dark: '#818cf8',    // Indigo-400
-        },
-        accent: {
-          DEFAULT: '#f59e42', // Orange-400
-          dark: '#fbbf24',    // Amber-400
-        },
-        secondary: {
-          DEFAULT: '#10b981', // Emerald-500
-          dark: '#34d399',    // Emerald-400
-        },
-        neutral: {
-          DEFAULT: '#f3f4f6', // Gray-100
-          dark: '#18181b',    // Gray-900
-        },
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
->>>>>>> 39967ba24a908fa881685039e5071215390c8290
       },
     },
   },
